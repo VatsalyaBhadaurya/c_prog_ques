e5cfaa2c6027dbcd24af1f46ebfbca20721b90da
Vatsalya Bhadaurya <150329198+VatsalyaBhadaurya@users.noreply.github.com>
2024-09-27 22:25:30 +0530

    Update w3_conditionals.c

--- w3_conditionals.c
+++ w3_conditionals.c
@@ -73,3 +73,26 @@ int main(int argc, char const *argv[])
 
     return 0;
 }
+
+// 4. Write a C program to find whether a given year is a leap year or not.
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a;
+    printf("Enter a year - ");
+    scanf("%d", &a);
+
+    if (a % 4 == 0)
+    {
+        printf("Leap year");
+    }
+
+    else
+    {
+        printf("Not a leap year");
+    }
+
+    return 0;
+}
