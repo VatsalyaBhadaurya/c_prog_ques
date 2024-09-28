29a37211ccdf3ede1757a36cfe25931d986134a8
Vatsalya Bhadaurya <150329198+VatsalyaBhadaurya@users.noreply.github.com>
2024-09-28 18:39:11 +0530

    Update w3_conditionals.c

--- w3_conditionals.c
+++ w3_conditionals.c
@@ -470,3 +470,55 @@ int main(int argc, char const *argv[])
 
     return 0;
 }
+
+// 21. Write a C program to read any day number in integer and display the day name in word format.
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a;
+    printf("Enter number for day - ");
+    scanf("%d", &a);
+    if (a == 1)
+    {
+        printf("Monday");
+    }
+
+    else if (a == 2)
+    {
+        printf("Tuesday");
+    }
+
+    else if (a == 3)
+    {
+        printf("Wednesday");
+    }
+
+    else if (a == 4)
+    {
+        printf("Thrusday");
+    }
+
+    else if (a == 5)
+    {
+        printf("Friday");
+    }
+
+    else if (a == 6)
+    {
+        printf("Saturday");
+    }
+
+    else if (a == 7)
+    {
+        printf("Sunday");
+    }
+
+    else
+    {
+        printf("enter correct number");
+    }
+
+    return 0;
+}
