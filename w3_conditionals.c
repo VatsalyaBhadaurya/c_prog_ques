b77e11bf506b9586b30196500fa9e9d31fd3155e
Vatsalya Bhadaurya <150329198+VatsalyaBhadaurya@users.noreply.github.com>
2024-09-28 10:42:26 +0530

    Update w3_conditionals.c

--- w3_conditionals.c
+++ w3_conditionals.c
@@ -121,3 +121,29 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
+// 6. Write a C program to read the value of an integer m and display the value of n is 1 when m is larger than 0, 0 when m is 0 and -1 when m is less than 0.
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a;
+    printf("Enter a number - ");
+    scanf("%d", &a);
+    if (a > 0)
+    {
+        printf("value of %d is 1 ", a);
+    }
+
+    else if (a = 0)
+    {
+        printf("Value of %d is 0", a);
+    }
+
+    else
+    {
+        printf("Value of %d is -1", a);
+    }
+
+    return 0;
+}
